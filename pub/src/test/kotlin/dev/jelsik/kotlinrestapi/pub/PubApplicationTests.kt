92ad188fcb19c84e0cd05c70d788a15b56ebe149
Jan Elsik <jelsik@trask.cz>
2022-10-15 14:38:25 +0200

    initial commit

---
+++ pub/src/test/kotlin/dev/jelsik/kotlinrestapi/pub/PubApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.jelsik.kotlinrestapi.pub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
